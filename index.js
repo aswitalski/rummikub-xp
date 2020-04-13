c29777a93579d33277fcda66ba691b25a76fd069
Aleksander Świtalski <aswitalski@opera.com>
2020-04-13 16:15:19 +0200

    Connect to the database at startup

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+const {Client} = require('pg');
+
+const db = new Client({connectionString: process.env.DATABASE_URL});
 
 express()
-  .use((request, response, next) => {
-    console.log(`${request.method} ${request.url}`);
-    return next();
-  })
-  .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.render('public/index.html'))
-  .listen(PORT, () => {
-    console.log('--------------------------------------------')
-    console.log(`  Starting Rummikub XP server on port ${PORT}`)
-    console.log('--------------------------------------------\n')
-  });
+    .use((request, response, next) => {
+      console.log(`${request.method} ${request.url}`);
+      return next();
+    })
+    .use(express.static(path.join(__dirname, 'public')))
+    .get('/', (req, res) => res.render('public/index.html'))
+    .listen(PORT, async () => {
+      console.log('--------------------------------------------')
+      console.log(`  Starting Rummikub XP server on port ${PORT}`)
+      console.log('--------------------------------------------')
+      try {
+        await db.connect();
+        console.log('=> Connected to the database');
+      } catch (e) {
+        console.error('=> ERROR: cannot connect to the database!');
+      }
+    });
