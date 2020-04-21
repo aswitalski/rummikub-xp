32dee2f0ffa8191d0457bf3d4fe7dea073e5f93b
Aleksander Świtalski <aswitalski@opera.com>
2020-04-22 01:55:18 +0200

    Render main screen

---
+++ public/js/render.js
@@ -0,0 +1,4 @@
+import Rummikub from './rummikub.js';
+
+opr.Toolkit.configure({});
+opr.Toolkit.render(Rummikub, document.body);
---
+++ public/js/rummikub.js
@@ -0,0 +1,24 @@
+export default class Rummikub extends opr.Toolkit.WebComponent {
+
+  render() {
+    return [
+      'main',
+      [
+        'button',
+        'Continue game',
+      ],
+      [
+        'button',
+        'New game',
+      ],
+      [
+        'button',
+        'Statistics',
+      ],
+      [
+        'button',
+        'Settings',
+      ],
+    ];
+  }
+}
